d66e55448252ad7dc1714d7e77b82d4543149fe9
chris <chris@5286ffc6-6c22-0410-b732-9076dd5b10c7>
2006-11-17 23:43:36 +0000

    model, view, and controller packages created and populated with stub classes

---
+++ src/controller/ControllerStub.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class ControllerStub {
+
+	// empty stub class
+	// this was just created to establish a standard repository structure
+	//
+	// James and Nick:
+	// start work on the controller here. You will probably
+	// want to rename this class to something actualy meaningful
+	
+}
