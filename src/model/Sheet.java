fb764da126ba62927a1cea8d0fd2c3a792e99df5
chris <chris@5286ffc6-6c22-0410-b732-9076dd5b10c7>
2006-11-19 23:16:58 +0000

    Jeks design recovery (see /trunk/design/Jeks.pdf)

--- src/model/Sheet.java
+++ src/model/Sheet.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.swing.table.DefaultTableModel;
 
 import com.eteks.parser.*;
@@ -22,9 +25,31 @@ implements ExpressionParameter
 		
 	}
 
-	public Object getParameterKey(String o) {
-		// TODO Auto-generated method stubfini
-		return null;
+	public Object getParameterKey(String parameter) {
+		
+		// return parameter if it's a legal cell name
+		
+		// regular expression to match
+		// 1 or more capital letters followed by
+		// 1 or more numbers
+		String regexCellName = "\\b[A-Z]+\\d+\\b";
+		
+		Pattern cellNamePattern = Pattern.compile(regexCellName);
+		
+		// check to see if the parameter is a legal cell name,
+		// and if so return it
+		if(cellNamePattern.matches(regexCellName, parameter)){
+
+			return parameter;
+			
+		}
+		else{
+			
+			return null;
+			
+		}
+		
+		
 	}
 
 	public Object getParameterValue(Object o) {
