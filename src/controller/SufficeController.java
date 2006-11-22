c6f3d5138745bc7c24de77ddae900a53b09a7ec8
james <james@5286ffc6-6c22-0410-b732-9076dd5b10c7>
2006-11-22 09:27:00 +0000

    Enabled simple saving/loading

--- src/controller/SufficeController.java
+++ src/controller/SufficeController.java
@@ -1,6 +1,21 @@
 package controller;
 
+import java.io.*;
+
 public static class SufficeController {
+	
+	// private WorkbookSerializer ws;
+	//
+	// Due to the simplicity of serialization,
+	// using a separate class to help save/load
+	// files seems unnecessary. However, it
+	// would separate concerns and be easier
+	// to expand or transfer.
+	//
+	// I'll leave it out for now, just to
+	// keep things localized for development,
+	// but should probably implement 
+	// WorkbookSerializer eventually.
 	
 	public SufficeController () {
 	}
@@ -9,13 +24,27 @@ public static class SufficeController {
 		return WorkbookDirector.construct();
 	}
 	
-	//Storing it as a workbook. Object OutputStream?
+	//Storing it as a workbook.
 	public static void save(Workbook w, OutputStream o) {
+        try {
+
+        	ObjectOutputStream oos = new ObjectOutputStream (o);
+    		oos.writeObject(w);
+        }
+        catch (Exception e) {}
+
+		// assuming the view will be responsible for selecting
+		// file (probably through JFileChooser, creating
+		// the OutputStream, and closing it after saving
 	}
 	
 	
 	//Retrieving the saved info, then casting it as a workbook. 
-	//Object InputStream?
 	public static void load(InputStream input) {
+		try {
+			ObjectInputStream ois = new ObjectInputStream (input);
+			Workbook w = (Workbook) ois.readObject();
+		}
+		catch (Exception e) {}
 	}
 }
